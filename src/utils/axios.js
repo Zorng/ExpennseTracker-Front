11ec35ec86fc1083c856acf732f52fb58c3a1a15
Zorng <ieangzorngl@gmail.com>
2025-07-26 16:24:50 +0700

    set up .env

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api', // Change to your API base URL if needed
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000/api',
 });
 
+console.log(`base url is ${import.meta.env.VITE_API_BASE_URL}`);
 // Attach JWT token to every request if available
 api.interceptors.request.use(
   (config) => {
